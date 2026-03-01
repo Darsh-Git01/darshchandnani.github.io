9400fd5383c56a00bcda6106d6d93cb26fd08810
Darsh Chandnani <darshchandnani@gmail.com>
2026-03-01 18:45:54 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // skip strict mode
+
+  output: "export",
+
+ 
+  images: {
+    unoptimized: true,
+  },
+
+ 
   reactStrictMode: false,
+
+
   env: {
-    googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
-  }
+    googleAnalyticsId:
+      process.env.NODE_ENV === "production"
+        ? process.env.GA_MEASUREMENT_ID
+        : "",
+  },
 };
 
 export default nextConfig;
