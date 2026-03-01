9dbc9497e67203605f163620ec2ab4d44e23f716
Darsh Chandnani <darshchandnani@gmail.com>
2026-03-01 17:15:12 +1100

    Update work.ts

--- app/constants/work.ts
+++ app/constants/work.ts
@@ -2,34 +2,27 @@ import * as THREE from "three";
 import { WorkTimelinePoint } from "../types";
 
 export const WORK_TIMELINE: WorkTimelinePoint[] = [
-  {
-    point: new THREE.Vector3(0, 0, 0),
-    year: '2014',
-    title: 'IIT Roorkee',
-    subtitle: 'Electrical Engineering',
+  { 
+    point: new THREE.Vector3(-4, -4, -3),
+    year: '2026',
+    title: 'Monash University',
+    subtitle: 'Masters in Artificial intelligence',
+    position: 'left',
+  },
+	{ point: new THREE.Vector3(0, 0, 0),
+    year: '2021-25',
+    title: 'Thapar Institute of Engineering and Technology',
+    subtitle: 'Bachelors in Mechatronics Engineering',
     position: 'right',
   },
   {
     point: new THREE.Vector3(-4, -4, -3),
-    year: '2016',
-    title: 'XPrep',
-    subtitle: 'Frontend Intern',
+    year: '2023-2024',
+    title: 'Codeasia',
+    subtitle: 'Data Analyst Intern',
     position: 'left',
   },
-  {
-    point: new THREE.Vector3(-3, -1, -6),
-    year: '2017',
-    title: 'Headout',
-    subtitle: 'Software Developer Intern',
-    position: 'left',
-  },
-  {
-    point: new THREE.Vector3(0, -1, -10),
-    year: '2018',
-    title: 'Cohesity',
-    subtitle: 'Member of Technical Staff',
-    position: 'left',
-  },
+
   {
     point: new THREE.Vector3(1, 1, -12),
     year: new Date().toLocaleDateString('default', { year: 'numeric' }),
